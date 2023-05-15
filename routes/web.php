6715747be40c963059271aea3c0681c38f4c35d7
felipe.bastos <felipe.bastos@meta.com.br>
2023-05-15 00:31:22 -0300

    criado as rotas de conteúdos do site com seus controllers e views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\CategoryController;
+use App\Http\Controllers\Site\BlogController;
+use App\Http\Controllers\Site\ContactController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +18,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::namespace('Site')->group(function () {
+    Route::get('/', [HomeController::class, '__invoke']);
+
+    Route::get('/produtos', [CategoryController::class, 'index']);
+    Route::get('/produtos/{slug}', [CategoryController::class, 'show']);
+
+    Route::get('/blog', [BlogController::class, '__invoke']);
+
+    Route::view('/sobre', 'site/about/index');
+
+    Route::get('/contato', [ContactController::class, 'index']);
+    Route::post('/contato', [ContactController::class, 'form']);
+});
